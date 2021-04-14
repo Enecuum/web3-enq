4786afee5af30cc8ae65b068f643281c4f66364e
kamighost1 <romires2000@gmail.com>
2021-04-14 19:59:34 +0300

    webpack is work

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,36 @@ const webpack = require("webpack");
 
 module.exports = {
     entry: {
-        enqweb3:'./prebuild/enqweb3.min.js',
+        enqweb3:'./dev/build.js',
+        ENQweb3lib:'./dev/buildEnq.js'
     },
     resolve: {
-        //modules: ["node_modules"],
+        modules: ["node_modules"],
         alias: {
-            '@':__dirname,
-            '@packs':path.resolve(__dirname,"packages")
+            process: "process/browser"
+        },
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "assert": require.resolve("assert"),
+            "https": require.resolve("https-browserify"),
+            "http": require.resolve("stream-http"),
+            "url": require.resolve("url/"),
+            "buffer": require.resolve("buffer/"),
+            "stream": require.resolve("stream-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
+            fs:false
         }
     },
     module: {
         rules: [
             {
-                test: /\.m?js$/,
+                test: /\.js$/,
                 use: {
                     loader: "babel-loader",
                     options: {
                         presets: [
                             [
                                 "@babel/preset-env",
-                                {
-                                    useBuiltIns: "entry",
-                                    corejs: 3,
-                                    targets: {
-                                        ie: 10,
-                                    },
-                                },
                             ],
                         ],
                         plugins: [
@@ -40,6 +44,14 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        })
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].min.js',
