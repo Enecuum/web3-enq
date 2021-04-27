623d332f6acad3013bac7185b6aaa5e13df0f41c
kamighost1 <romires2000@gmail.com>
2021-04-27 15:08:42 +0300

    fix

--- packages/web3-enq-net/src/index.js
+++ packages/web3-enq-net/src/index.js
@@ -215,7 +215,7 @@ const Net = function Net(web) {
                 nonce: obj.nonce || Math.floor(Math.random() * 1e10)
             };
             if (obj.data) {
-                tx.data = await web.Utils.dfo(obj.data)
+                tx.data = obj.data
             } else {
                 tx.data = '';
             }
